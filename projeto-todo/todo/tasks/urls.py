8d93ce7712f08aadbe35d1b9d3e13d020a9480f6
Augusto Elias <augusto.elias@mycit.ie>
2023-05-17 14:41:46 +0100

    STEP 3

--- projeto-todo/todo/tasks/urls.py
+++ projeto-todo/todo/tasks/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('helloworld/', views.helloworld),
+    path('', views.taskList, name='task-list'),  # empty url will be the 'home'
+    path('yourname/<str:name>', views.yourName, name='your-name'),  # that will allow to receive a string
+
 ]
--- projeto-todo/todo/tasks/views.py
+++ projeto-todo/todo/tasks/views.py
@@ -3,4 +3,18 @@ from django.shortcuts import render
 
 
 def helloworld(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse ('Hello World!')
+
+
+def taskList(request):
+    return render (request, 'tasks/list.html')
+# REMEMBER view always need request and return
+# RENDER is a built-in function that needs 2 parameters: a request and a template
+# we are going to create the template called list.html
+
+
+def yourName(request, name):
+    return render (request, 'tasks/yourname.html', {'name': name})
+# 'name' could be any other word
+# that is returning the request , template , argument
+
