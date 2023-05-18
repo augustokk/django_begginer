bfa02f162c6cbf766f85b35eef88fe9d95252b90
Augusto Elias <augusto.elias@mycit.ie>
2023-05-18 15:32:24 +0100

    STEP 7

--- projeto-todo/todo/tasks/models.py
+++ projeto-todo/todo/tasks/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model): #inherit data from the file models of the class Model
+
+    # constant with values in tuple 
+    STATUS= (
+        ('Doing', 'Doing'),
+        ('Done', 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    # inherit data from the file models of the class Charfield
+    # charfield means that will accpt any element from charset
+    # that needs to receive a size argument, we set as 255
+
+    description = models.TextField()
+    # use textfield, because there is no max length, better choice for description
+
+    done = models.CharField(
+        max_length=10, 
+        choices = STATUS,)
+    # the value will be determined by the constant STATUS
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    # that will automatically store the new date and time when a new object is created
+
+    updated_at =  models.DateTimeField(auto_now=True)
+    # that will automatically store the date and time when a new object is updated
+
+# function to show title of task in the django admin website
+    def __str__(self):
+        return self.title
