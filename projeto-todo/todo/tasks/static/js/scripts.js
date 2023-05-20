2b3bead37a4a21d801b1ad1395349a66e918c440
Augusto Elias <augusto.elias@mycit.ie>
2023-05-20 11:23:02 +0100

    STEP 12

--- projeto-todo/todo/tasks/static/js/scripts.js
+++ projeto-todo/todo/tasks/static/js/scripts.js
@@ -1,1 +1,23 @@
-console.log('working!');
+$(document).ready(function() {
+    // create a variable that will select that delete element 
+    var deleteBtn = $('.delete-btn');
+
+    // function that activates when click with an parameter (e)vent
+    $(deleteBtn).on('click', function(e) {
+        
+        // that will prevent that delete button to delete straigth away
+        e.preventDefault();
+
+        // create a link when we want to delete the task
+        var delLink =$(this).attr('href');
+
+        // create message to check if wants delete
+        var result = confirm('Do you want to delete this task?')
+
+        // if user confirms that wants to delete
+        if(result){
+            window.location.href = delLink
+        }
+
+    });
+});
--- projeto-todo/todo/tasks/views.py
+++ projeto-todo/todo/tasks/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from .forms import TaskForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 def helloworld(request):
@@ -12,25 +13,38 @@ def helloworld(request):
 def taskList(request):
     # that will collect all objects from model Task from database
     # that will order the data by creation date
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+
+    # that will create a pagination that will display only 3 tasks per page
+    # avoid overuse of the database, user-friendly not populating too much the pages
+    paginator = Paginator(tasks_list, 3)
+
+    # create a variable to set the url for the page
+    # then the tasks to be shown will be depending of the page requested
+    # that allows to show the correct number of tasks (3) on the correct page
+    page =request.GET.get('page')
+    tasks = paginator.get_page(page)
+
+    # REMEMBER view always need request and return
+    # RENDER is a built-in function that needs 2 parameters: a request and a template
+    # we are going to create the template called list.html
+    # we add a dictionary to send all that objects to the template
     return render (request, 'tasks/list.html', {'tasks' : tasks})
-# REMEMBER view always need request and return
-# RENDER is a built-in function that needs 2 parameters: a request and a template
-# we are going to create the template called list.html
-# we add a dictionary to send all that objects to the template
+
 
 def yourName(request, name):
+    # 'name' could be any other word
+    # that is returning the request , template , argument
     return render (request, 'tasks/yourname.html', {'name': name})
-# 'name' could be any other word
-# that is returning the request , template , argument
 
 
+# that will receive 2 arguments request and unique id provided by django database
 def taskView(request, id): 
-    task = get_object_or_404(Task, pk=id)
-    return render (request, 'tasks/task.html', {'task' : task})
-    # that will receive 2 arguments request and unique id provided by django database
     # that will get all objects with id as primary key from database
     # if there is none object with id, return a 404 error (safety reason)
+    task = get_object_or_404(Task, pk=id)
+    return render (request, 'tasks/task.html', {'task' : task})
+
 
 def newTask(request):
     # that if will select the option to add task 
@@ -92,6 +106,7 @@ def editTask(request, id):
         
     else:
         return render(request, 'tasks/edittask.html', {'form' : form, 'task': task})
+    
     
 # that function needs to receive the id to be able to delete the specific task
 def deleteTask(request, id):
